42b85b4ea54d22f290049575385c73b8d937d2a3
yusuf erdogan <yusuffx03@gmail.com>
2022-02-11 18:44:39 +0300

    added controller for root

--- src/main/java/com/example/hrms/controllers/IndexController.java
+++ src/main/java/com/example/hrms/controllers/IndexController.java
@@ -1,12 +1,14 @@
 package com.example.hrms.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@RestController()
+@RequestMapping("")
 public class IndexController {
 
-    @GetMapping("")
+    @GetMapping({"","/"})
     String getIndex() {
         return """
                 Welcome to HRMS Spring application
@@ -25,7 +27,7 @@ public class IndexController {
     @GetMapping("/error")
     String getError() {
         return """
-                An Error occured please go to
+                An Error occured please go to 
                  https://human-resources-spring-boot.herokuapp.com
                 """;
     }
