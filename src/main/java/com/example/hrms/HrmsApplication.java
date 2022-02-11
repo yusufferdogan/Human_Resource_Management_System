9a0adbced609117f6f448cde092771840df31790
yusuf erdogan <yusuffx03@gmail.com>
2022-02-11 20:33:32 +0300

    swagger added

--- src/main/java/com/example/hrms/HrmsApplication.java
+++ src/main/java/com/example/hrms/HrmsApplication.java
@@ -1,13 +1,30 @@
 package com.example.hrms;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HrmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HrmsApplication.class, args);
+    }
+    @Bean
+    public OpenAPI openApiConfig() {
+        return new OpenAPI().info(apiInfo());
+    }
+
+    public Info apiInfo() {
+        Info info = new Info();
+        info
+                .title("Live Code API")
+                .description("Live Code System Swagger Open API")
+                .version("v1.0.0");
+        return info;
     }
 
 }
