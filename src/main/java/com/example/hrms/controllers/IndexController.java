48a453a9537c00b3a94a0030452742c58305e876
yusuf erdogan <yusuffx03@gmail.com>
2022-02-11 19:15:54 +0300

    changed pom.xml

--- src/main/java/com/example/hrms/controllers/IndexController.java
+++ src/main/java/com/example/hrms/controllers/IndexController.java
@@ -1,15 +1,10 @@
 package com.example.hrms.controllers;
-
 import com.example.hrms.core.utilities.results.SuccessDataResult;
-import com.example.hrms.models.Experience;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 @RestController()
 @RequestMapping("")
 public class IndexController {
@@ -26,7 +21,6 @@ public class IndexController {
                 /experience
                 /job_advertisement
                 /task_masters
-                 
                 """;
         SuccessDataResult<String> results =  new SuccessDataResult<>
                 (str,"Root Page");
@@ -36,8 +30,8 @@ public class IndexController {
     @GetMapping("/error")
     ResponseEntity<?> getError() {
         String str = """
-                An Error occured please go to 
-                 https://human-resources-spring-boot.herokuapp.com
+                An Error occured please go to
+                https://human-resources-spring-boot.herokuapp.com
                 """;
         SuccessDataResult<String> results =  new SuccessDataResult<>
                 (str,"Root Page");
